0aa3ec36121160db892057efca6cc7029253840e
mingkunshi <mingkunshi@mingkunshideMBP.fios-router.home>
2020-10-07 19:33:01 -0400

    2020/10/06

---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import { fetchCountries } from '../../api';
+
+import styles from './CountryPicker.module.css';
+
+const Countries = ({ handleCountryChange }) => {
+  const [countries, setFetchedCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries());
+    };
+
+    fetchAPI();
+  }, []);
+
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect
+        defaultValue=''
+        onChange={(e) => handleCountryChange(e.target.value)}
+      >
+        <option value='global'>Global</option>
+        {countries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
+};
+
+export default Countries;
