480dfd97732e98ced4f0fcbde30da6823b5b34b5
mingkunshi <mingkunshi@mingkunshideMBP.fios-router.home>
2020-10-11 03:37:17 -0400

    Updates

---
+++ precache-manifest.836c86949558a78a740c42cefbb26912.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218d079fdfc8c4889445d890f46b4a42",
+    "url": "/Covid19_Tracker3/index.html"
+  },
+  {
+    "revision": "d862df4cb50a1bd0d90e",
+    "url": "/Covid19_Tracker3/static/css/main.2904aa40.chunk.css"
+  },
+  {
+    "revision": "5683da4653353d03cd8b",
+    "url": "/Covid19_Tracker3/static/js/2.a88e5d18.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid19_Tracker3/static/js/2.a88e5d18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d862df4cb50a1bd0d90e",
+    "url": "/Covid19_Tracker3/static/js/main.28909add.chunk.js"
+  },
+  {
+    "revision": "d839b2871f1a048b8913",
+    "url": "/Covid19_Tracker3/static/js/runtime-main.4feddfe6.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19_Tracker3/static/media/image.d7265326.png"
+  }
+]);
